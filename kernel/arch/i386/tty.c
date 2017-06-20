e236ba2f7c81b0a6ac3551bc0d21ba43a2f99252
Yorick Rommers <yjpmrommers@gmail.com>
2017-06-20 19:23:47 +0200

    Implement terminal scrolling in tty

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -38,13 +38,32 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
+void terminal_scroll(void) {
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			const size_t index2 = (y + 1) * VGA_WIDTH + x;
+			terminal_buffer[index] = terminal_buffer[index2];
+		}
+	}
+}
+
 void terminal_putchar(char c) {
 	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
+	if (c == '\n') {
+		terminal_row++;
 		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
+	} else {
+		terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+		terminal_column++;
+	}
+	if (terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+	}
+	if (terminal_row == VGA_HEIGHT) {
+		// terminal_row = 0;
+		terminal_scroll();
+		terminal_row--;
 	}
 }
 
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,5 +4,13 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+
+	terminal_writestring("Hello, kernel World!\n");
+
+	terminal_writestring("Hello line two\n");
+	for (size_t i = 0; i < 100; i++) {
+		terminal_writestring("Hello multiple lines\n");
+	}
+	terminal_writestring("Here is another final string\n");
+	terminal_writestring("Here is the actual final string\n");
 }
