f13d84ef9991670468803a1cea2a5b236f594884
Yorick Rommers <yjpmrommers@gmail.com>
2017-06-20 12:27:22 +0200

    Rename to colonel

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/colonel.kernel isodir/boot/colonel.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "colonel" {
+	multiboot /boot/colonel.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o colonel.iso isodir
