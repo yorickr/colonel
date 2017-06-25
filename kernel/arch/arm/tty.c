8161c5431a420a142f301354c5ab5d5787f50d3f
Yorick Rommers <yjpmrommers@gmail.com>
2017-06-25 15:52:09 +0200

    Made tty.c for arm arch using uart

---
+++ kernel/arch/arm/tty.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <kernel/tty.h>
+
+#include <kernel/uart.h>
+
+void terminal_initialize(void) {
+	uart_init();
+}
+
+void terminal_putchar(char c) {
+	uart_putc(c);
+}
+
+void terminal_write(const char* data, size_t size) {
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data) {
+	terminal_write(data, strlen(data));
+}
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,26 +3,30 @@
 #include <kernel/uart.h>
 #include <kernel/tty.h>
 
+
+#if defined(__cplusplus)
+extern "C" /* Use C linkage for kernel_main. */
+#endif
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
 	// Declare as unused
 	(void) r0;
 	(void) r1;
 	(void) atags;
 
-	uart_init();
-	uart_puts("Hello, kernel World!\r\n");
-
-	while (1)
-		uart_putc(uart_getc());
-
-	// terminal_initialize();
+	// uart_init();
+	// uart_puts("Hello, kernel World!\r\n");
 	//
-	// terminal_writestring("Hello, kernel World!\n");
-	//
-	// terminal_writestring("Hello line two\n");
-	// for (size_t i = 0; i < 100; i++) {
-	// 	terminal_writestring("Hello multiple lines\n");
-	// }
-	// terminal_writestring("Here is another final string\n");
-	// terminal_writestring("Here is the actual final string\n");
+	// while (1)
+	// 	uart_putc(uart_getc());
+
+	terminal_initialize();
+
+	terminal_writestring("Hello, kernel World!\n");
+	
+	terminal_writestring("Hello line two\n");
+	for (size_t i = 0; i < 100; i++) {
+		terminal_writestring("Hello multiple lines\n");
+	}
+	terminal_writestring("Here is another final string\n");
+	terminal_writestring("Here is the actual final string\n");
 }
